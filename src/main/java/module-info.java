17015004353d662fb28967844a7908ba5be4696d
JiaweiMao <jiaweimao2019@outlook.com>
2025-07-04 09:29:37 +0800

    add additional exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module pdk.util {
+
+    // 3rd party
     requires org.jspecify;
     requires commons.math3;
 
     exports pdk.util;
+    exports pdk.util.data;
+    exports pdk.util.data.func;
     exports pdk.util.exception;
     exports pdk.util.io;
     exports pdk.util.tuple;
