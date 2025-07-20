9d586c147ea2d52c2e916c6ecf71104718734380
JiaweiMao <jiaweimao2019@outlook.com>
2025-07-20 20:38:56 +0800

    add MathUtils

--- src/main/java/pdk/util/MathUtils.java
+++ src/main/java/pdk/util/MathUtils.java
@@ -1,5 +1,9 @@
 package pdk.util;
 
+import org.apache.commons.math3.stat.StatUtils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collection;
 
 import static pdk.util.ArgUtils.checkArgument;
@@ -14,6 +18,7 @@ import static pdk.util.ArgUtils.checkNotNull;
  */
 public final class MathUtils {
 
+    private MathUtils() {}
 
     static final double[] factorial = new double[128];
     static final double[] logFactorial = new double[128];
@@ -45,6 +50,43 @@ public final class MathUtils {
      */
     public static double lnFactorial(int n) {
         return logFactorial[n];
+    }
+
+    /**
+     * Return true if the given integer is odd
+     *
+     * @param x integer
+     * @return true if the integer is odd
+     */
+    public static boolean isOdd(int x) {
+        return x % 2 == 1;
+    }
+
+    /**
+     * Return the next largest power of two
+     * <ul>
+     *     <li>negative integer will return 0</li>
+     *     <li>0 -> 0</li>
+     *     <li>1 -> 1</li>
+     *     <li>2 -> 2</li>
+     *     <li>3 -> 4</li>
+     *     <li>...</li>
+     * </ul>
+     *
+     * @param value an integer
+     * @return next largest power of two
+     * @since 2024-07-01 ⭐
+     */
+    public static int nextPowerOfTwo(int value) {
+        int n = value;
+        n--;
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        n++;
+        return n;
     }
 
     /**
@@ -112,6 +154,37 @@ public final class MathUtils {
         return sum;
     }
 
+    /**
+     * The weighted sum of the entries in the specified input array.
+     * Uses the formula,
+     * <pre>
+     *  weighted sum = &Sigma;(values[i] * weights[i])
+     * </pre>
+     *
+     * @param values  the input array
+     * @param weights the weight array
+     * @return weighted sum
+     */
+    public static double sum(final double[] values, final double[] weights) {
+        checkNotNull(values);
+        checkNotNull(weights);
+
+        if (values.length != weights.length) {
+            throw new IllegalArgumentException("The length of values and weights should be the same.");
+        }
+
+        double sum = 0.;
+        for (int i = 0; i < values.length; i++) {
+            double weight = weights[i];
+            if (Double.isNaN(weight))
+                throw new IllegalArgumentException("NaN weight at " + i);
+            if (Double.isInfinite(weight))
+                throw new IllegalArgumentException("Infinite value at " + i);
+            sum += values[i] * weights[i];
+        }
+        return sum;
+    }
+
     public static double mean(Collection<Double> values) {
         checkArgument(values != null && !values.isEmpty());
 
@@ -120,24 +193,23 @@ public final class MathUtils {
     }
 
     /**
-     * Return variance of a sample dataset
+     * Return the mean of the values.
      *
-     * @param values data set
-     * @return variance of the data.
+     * @param values a Double array.
+     * @return mean of the array values.
      */
-    public static double variance(final Collection<Double> values) {
-        return variance(values, true);
+    public static double mean(Double[] values) {
+        return sum(values) / values.length;
     }
 
     /**
-     * Return variance of the data
+     * Return the mean value of a given dataset
      *
-     * @param values data set
-     * @return variance of the data.
+     * @param values dataset
+     * @return mean value
      */
-    public static double variance(final Collection<Double> values, boolean isBias) {
-        double mean = mean(values);
-        return variance(values, mean, isBias);
+    public static double mean(double[] values) {
+        return sum(values) / values.length;
     }
 
     /**
@@ -166,6 +238,27 @@ public final class MathUtils {
             } else
                 return sum / len;
         }
+    }
+
+    /**
+     * Return variance of a sample dataset
+     *
+     * @param values data set
+     * @return variance of the data.
+     */
+    public static double variance(final Collection<Double> values) {
+        return variance(values, true);
+    }
+
+    /**
+     * Return variance of the data
+     *
+     * @param values data set
+     * @return variance of the data.
+     */
+    public static double variance(final Collection<Double> values, boolean isBias) {
+        double mean = mean(values);
+        return variance(values, mean, isBias);
     }
 
     /**
@@ -198,6 +291,40 @@ public final class MathUtils {
     }
 
     /**
+     * Calculate the variance of the double array.
+     *
+     * @param values double array
+     * @param bias   Whether bias correction is applied when computing the value of the statistics.
+     * @return variance of the data.
+     */
+    public static double variance(final double[] values, boolean bias) {
+        double mean = mean(values);
+        return variance(values, mean, 0, values.length, bias);
+    }
+
+    /**
+     * Calculate the variance of the double array.
+     *
+     * @param values double array
+     * @param mean   mean
+     * @return variance of the data.
+     */
+    public static double variance(final double[] values, double mean) {
+        return variance(values, mean, 0, values.length, true);
+    }
+
+    /**
+     * Calculate the variance of the double array.
+     *
+     * @param values double array
+     * @return variance of the data.
+     */
+    public static double variance(final double[] values) {
+        double mean = mean(values);
+        return variance(values, mean, 0, values.length, true);
+    }
+
+    /**
      * Return the standard deviation of given double values
      *
      * @param values double values
@@ -205,5 +332,77 @@ public final class MathUtils {
      */
     public static double standardDeviation(Collection<Double> values) {
         return Math.sqrt(variance(values));
+    }
+
+    /**
+     * Return the standard deviation of given sample data.
+     *
+     * @param values values
+     * @return standard deviation.
+     */
+    public static double standardDeviation(final double[] values) {
+        return Math.sqrt(variance(values));
+    }
+
+    /**
+     * Return the relative standard deviation (RSD) of given double array.
+     *
+     * @param values a {@link Double} array.
+     * @return relative standard deviation of the double array.
+     */
+    public static double rsd(double[] values) {
+        double mean = mean(values);
+        double variance = variance(values, mean, 0, values.length, true);
+        return Math.sqrt(variance) / mean;
+    }
+
+    /**
+     * Return the relative standard deviation (RSD) of given double values
+     *
+     * @param values double values
+     * @return relative standard deviation
+     */
+    public static double rsd(Collection<Double> values) {
+        double mean = mean(values);
+        double variance = variance(values, mean, true);
+        return Math.sqrt(variance) / mean;
+    }
+
+    /**
+     * Rounds a double value to the wanted number of decimal places.
+     *
+     * @param value         the value to scale
+     * @param decimalPlaces the number of decimal places
+     * @return a scaled double to the indicated decimal places
+     */
+    public static double round(double value, int decimalPlaces) {
+        checkArgument(decimalPlaces >= 0);
+
+        BigDecimal bd = BigDecimal.valueOf(value).setScale(decimalPlaces, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    /**
+     * Find the first quantile (1/4) of an array
+     *
+     * @param values double array
+     * @return the first quantile
+     */
+    public static double q1(double[] values) {
+        return StatUtils.percentile(values, 25);
+    }
+
+    /**
+     * Find the median of an array
+     *
+     * @param values double array
+     * @return median
+     */
+    public static double median(double[] values) {
+        return StatUtils.percentile(values, 50);
+    }
+
+    public static double q3(double[] values) {
+        return StatUtils.percentile(values, 75);
     }
 }
