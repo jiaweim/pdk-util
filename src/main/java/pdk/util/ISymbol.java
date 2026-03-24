c5c956d50fef0ac8b42618175c6d0834b27b3b91
JiaweiMao <jiaweimao2019@outlook.com>
2026-03-24 16:07:04 +0800

    add ISymbol

---
+++ src/main/java/pdk/util/ISymbol.java
@@ -0,0 +1,16 @@
+package pdk.util;
+
+/**
+ * For any object with a symbol.
+ *
+ * @author Jiawei Mao
+ * @version 1.0.0
+ * @since 21 Jun 2024, 10:37 AM
+ */
+public interface ISymbol {
+
+    /**
+     * @return a short name of the object.
+     */
+    String getSymbol();
+}
